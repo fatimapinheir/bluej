5642078097501bcc0fc34e4210dc4b33a1b0ecfa
Fátima Pinheiro <30393500+fatimapinheir@users.noreply.github.com>
2021-05-12 19:19:43 -0300

    Create Helloword.java

---
+++ Helloword.java
@@ -0,0 +1,19 @@
+  /**
+   * Teste da disciplina.
+   * 
+   * @author (Fatima Pinheiro) 
+   * @version (1.0 10/05/2021)
+   */
+  public class Helloword{
+  //Declaração de variáveis
+  private String nome;
+  
+  //Construtor da classe
+      public Helloword (String n){
+          nome = n;
+         System.out.print(nome);
+      }
+ //Teste Commit Alison
+ //teste commit 2
+  // 
+  }
